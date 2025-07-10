a7454be398f218d980108159dc8ea3e77a7330c7
IWAJU14 <develop4iwaju@gmail.com>
2025-07-10 09:38:56 +0100

    Finale 2.2

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -110,7 +110,18 @@ class AdminSeeder extends Seeder
             'nom' => 'Sofitel Hôtel Ibis Cotonou',
             'description' => 'Un hôtel de luxe situé à proximité de l\'aéroport avec des services haut de gamme.',
             'localisation' => 'Sofitel, Boulevard de la Marina, Cotonou Bénin',
-            'images' => ['infrastructures/sofitel1.jpg'],
+            'images' => [
+                'infrastructures/sofitel1.png',
+                'infrastructures/sofitel2.png',
+                'infrastructures/sofitel3.png',
+                'infrastructures/sofitel4.png',
+                'infrastructures/sofitel5.png',
+                'infrastructures/sofitel6.png',
+                'infrastructures/sofitel7.png',
+                'infrastructures/sofitel8.png',
+                'infrastructures/sofitel9.png',
+                'infrastructures/sofitel10.png',
+            ],
             'type' => 'hotel',
             'caracteristiques' => [
                 'capacite' => 150,
@@ -129,8 +140,16 @@ class AdminSeeder extends Seeder
             'acteur_touristique_id' => $acteur1->id,
             'nom' => 'Plage de Fidjrossè',
             'description' => 'Plage prisée de Cotonou, idéale pour les promenades au coucher du soleil.',
-            'localisation' => 'Plage de Fidjrossè, Route des Pêches, Fidjrossè, Cotonou',
-            'images' => ['infrastructures/fidjrosse1.jpg'],
+            'localisation' => 'Plage de Fidjrossè, Fidjrossè, Cotonou',
+            'images' => [
+                'infrastructures/fidjrosse1.png',
+                'infrastructures/fidjrosse2.png',
+                'infrastructures/fidjrosse3.png',
+                'infrastructures/fidjrosse4.png',
+                'infrastructures/fidjrosse5.png',
+                'infrastructures/fidjrosse6.png',
+                'infrastructures/fidjrosse7.png',
+            ],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 0,
@@ -146,7 +165,10 @@ class AdminSeeder extends Seeder
             'nom' => 'Chez Clarisse Fidjrossè',
             'description' => 'Restaurant local célèbre pour ses poissons braisés et mets béninois.',
             'localisation' => 'Chez Clarisse, Fidjrossè plage, Cotonou',
-            'images' => ['infrastructures/chezclarisse.jpg'],
+            'images' => [
+                'infrastructures/clarisse1.png',
+                'infrastructures/clarisse2.png'
+            ],
             'type' => 'restaurant',
             'caracteristiques' => [
                 'capacite' => 60,
@@ -163,7 +185,15 @@ class AdminSeeder extends Seeder
             'nom' => 'Taxi Vite Cotonou',
             'description' => 'Service de transport rapide en taxi urbain dans les principales artères de Cotonou.',
             'localisation' => 'Ganhi, Cotonou',
-            'images' => ['infrastructures/taxi-vite.jpg'],
+            'images' => [
+                'infrastructures/taxi1.png',
+                'infrastructures/taxi2.png',
+                'infrastructures/taxi3.png',
+                'infrastructures/taxi4.png',
+                'infrastructures/taxi5.png',
+                'infrastructures/taxi6.png',
+                'infrastructures/taxi7.png'
+            ],
             'type' => 'transport',
             'caracteristiques' => [
                 'prix_moyen' => 1500,
@@ -179,7 +209,17 @@ class AdminSeeder extends Seeder
             'nom' => 'Bus Touristique Cotonou',
             'description' => 'Circuit touristique en bus climatisé pour découvrir les monuments de Cotonou.',
             'localisation' => 'Bénin',
-            'images' => ['infrastructures/bus-tour.jpg'],
+            'images' => [
+                'infrastructures/bus1.png',
+                'infrastructures/bus2.png',
+                'infrastructures/bus3.png',
+                'infrastructures/bus4.png',
+                'infrastructures/bus5.png',
+                'infrastructures/bus6.png',
+                'infrastructures/bus7.png',
+                'infrastructures/bus8.png',
+                'infrastructures/bus9.png'
+            ],
             'type' => 'transport',
             'caracteristiques' => [
                 'capacite' => 30,
@@ -196,7 +236,15 @@ class AdminSeeder extends Seeder
             'nom' => 'Place de l’Amazone',
             'description' => 'Esplanade monumentale abritant la statue de l\'Amazone, symbole historique du Bénin.',
             'localisation' => 'Place de l’Amazone, Cotonou, Bénin',
-            'images' => ['infrastructures/amazone1.jpg'],
+            'images' => [
+                'infrastructures/amazone1.png',
+                'infrastructures/amazone2.png',
+                'infrastructures/amazone3.png',
+                'infrastructures/amazone4.png',
+                'infrastructures/amazone5.png',
+                'infrastructures/amazone6.png',
+                'infrastructures/amazone7.png'
+            ],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 0,
@@ -213,7 +261,10 @@ class AdminSeeder extends Seeder
             'nom' => 'Place Bio Guéra',
             'description' => 'Statue équestre rendant hommage au héros de la résistance du Nord béninois.',
             'localisation' => 'Monument Bio Guera, Aéroport de Cadjehoun, Cotonou, Bénin',
-            'images' => ['infrastructures/bioguera1.jpg'],
+            'images' => [
+                'infrastructures/bioguera1.png',
+                'infrastructures/bioguera2.png'
+            ],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 0,
@@ -230,7 +281,7 @@ class AdminSeeder extends Seeder
             'nom' => 'Place Bulgarie',
             'description' => 'Point névralgique et dynamique de la circulation à Cotonou, symbole urbain.',
             'localisation' => 'Place Bulgarie, Cotonou, Bénin',
-            'images' => ['infrastructures/bulgarie1.jpg'],
+            'images' => ['infrastructures/bulgarie.png'],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 0,
@@ -247,7 +298,21 @@ class AdminSeeder extends Seeder
             'nom' => 'Ganvié Village Lacustre',
             'description' => 'Village construit sur pilotis au cœur du lac Nokoué, expérience unique.',
             'localisation' => 'Ganvié, Lac Nokoué',
-            'images' => ['infrastructures/ganvie1.jpg'],
+            'images' => [
+                'infrastructures/ganvie1.png',
+                'infrastructures/ganvie2.png',
+                'infrastructures/ganvie3.png',
+                'infrastructures/ganvie4.png',
+                'infrastructures/ganvie5.png',
+                'infrastructures/ganvie6.png',
+                'infrastructures/ganvie7.png',
+                'infrastructures/ganvie8.png',
+                'infrastructures/ganvie9.png',
+                'infrastructures/ganvie10.png',
+                'infrastructures/ganvie11.png',
+                'infrastructures/ganvie12.png',
+                'infrastructures/ganvie13.png'
+            ],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 3000,
@@ -263,7 +328,15 @@ class AdminSeeder extends Seeder
             'nom' => 'Le Livingstone',
             'description' => 'Restaurant emblématique à ambiance coloniale avec une carte internationale.',
             'localisation' => 'Le Livingstone, Boulevard Saint Michel, Cotonou, Bénin',
-            'images' => ['infrastructures/livingstone.jpg'],
+            'images' => [
+                'infrastructures/livingstone1.png',
+                'infrastructures/livingstone2.png',
+                'infrastructures/livingstone3.png',
+                'infrastructures/livingstone4.png',
+                'infrastructures/livingstone5.png',
+                'infrastructures/livingstone6.png',
+                'infrastructures/livingstone7.png'
+            ],
             'type' => 'restaurant',
             'caracteristiques' => [
                 'capacite' => 100,
@@ -281,7 +354,26 @@ class AdminSeeder extends Seeder
             'nom' => 'Résidence Tata Somba',
             'description' => 'Hébergement traditionnel inspiré de l\'architecture du Nord Bénin.',
             'localisation' => 'Hôtel Tata Somba, Natitingou, Bénin',
-            'images' => ['infrastructures/tatasomba.jpg'],
+            'images' => [
+                'infrastructures/somba1.png',
+                'infrastructures/somba2.png',
+                'infrastructures/somba3.png',
+                'infrastructures/somba4.png',
+                'infrastructures/somba5.png',
+                'infrastructures/somba6.png',
+                'infrastructures/somba7.png',
+                'infrastructures/somba8.png',
+                'infrastructures/somba9.png',
+                'infrastructures/somba10.png',
+                'infrastructures/somba11.png',
+                'infrastructures/somba12.png',
+                'infrastructures/somba13.png',
+                'infrastructures/somba14.png',
+                'infrastructures/somba15.png',
+                'infrastructures/somba16.png',
+                'infrastructures/somba17.png',
+                'infrastructures/somba18.png'
+            ],
             'type' => 'hotel',
             'caracteristiques' => [
                 'capacite' => 25,
@@ -297,7 +389,14 @@ class AdminSeeder extends Seeder
             'nom' => 'Temple des Pythons',
             'description' => 'Lieu sacré du culte vaudou, célèbre pour sa coexistence entre les pythons vivants et la spiritualité traditionnelle.',
             'localisation' => 'Temple des Pythons, Ouidah, Bénin',
-            'images' => ['infrastructures/temple-pythons.jpg'],
+            'images' => [
+                'infrastructures/python1.png',
+                'infrastructures/python2.png',
+                'infrastructures/python3.png',
+                'infrastructures/python4.png',
+                'infrastructures/python5.png',
+                'infrastructures/python6.png'
+            ],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 1000,
@@ -315,7 +414,14 @@ class AdminSeeder extends Seeder
             'nom' => 'Grande Mosquée de Porto-Novo',
             'description' => 'Mosquée historique construite entre 1912 et 1925, alliant style afro-brésilien et héritage islamique.',
             'localisation' => 'Grande Mosquée de Porto-Novo, Centre-ville, Porto-Novo, Bénin',
-            'images' => ['infrastructures/mosquee-porto.jpg'],
+            'images' => [
+                'infrastructures/mosque1.png',
+                'infrastructures/mosque2.png',
+                'infrastructures/mosque3.png',
+                'infrastructures/mosque4.png',
+                'infrastructures/mosque5.png',
+                'infrastructures/mosque6.png'
+            ],
             'type' => 'attraction',
             'caracteristiques' => [
                 'prix' => 0,
